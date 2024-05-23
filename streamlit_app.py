998c70784b232a2d51ce6331fc5abaf3c7395a71
gouthami24 <gouthami.santhanam@gmail.com>
2024-05-23 12:45:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,24 +14,46 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 st.title('RAG Search')
 
-from langchain_openai import ChatOpenAI
-llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature = 0.5)
-response = llm.invoke("What is LLM")
+#from langchain_openai import ChatOpenAI
+#llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-3.5-turbo", temperature = 0.5)
+#response = llm.invoke("What is LLM")
 #print(response)
-st.write("Before Search using RAG, RESPONSE : ", response.content)
+#st.write("Before Search using RAG, RESPONSE : ", response.content)
 
-def get_docs():
-    loader = WebBaseLoader('https://www.techtarget.com/whatis/feature/Foundation-models-explained-Everything-you-need-to-know')
-    docs = loader.load()
+if not openai_api_key.startswith('sk-'):
+   st.warning('Please enter your OpenAI API key!', icon='⚠')
+if openai_api_key.startswith('sk-'):
+    uploaded_file=st.file_uploader("Upload text files", type=["txt", "pdf"], accept_multiple_files=False)
+    user_question=st.text_input("Enter your question:")
+    
+    def get_docs_from_file(uploaded_file):
+        docs = []
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+             tmp_file.write(uploaded_file.read())
+             tmp_file_path = tmp_file.name
+        # Load the document using TextLoader
+        loader = TextLoader(tmp_file_path)
+        docs.extend(loader.load())
+    
+        # Split the document into chunks
+        text_splitter = RecursiveCharacterTextSplitter(
+             chunk_size=200,
+             chunk_overlap=20
+        )
+        split_docs = text_splitter.split_documents(docs)
+        return split_docs
+        
+#def get_docs():
+#    loader = WebBaseLoader('https://www.techtarget.com/whatis/feature/Foundation-models-explained-Everything-you-need-to-know')
+#    docs = loader.load()
 
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=200,
-        chunk_overlap=20
-    )
+#    text_splitter = RecursiveCharacterTextSplitter(
+#        chunk_size=200,
+#        chunk_overlap=20
+#    )
 
-    splitDocs = text_splitter.split_documents(docs)
-
-    return splitDocs
+#    splitDocs = text_splitter.split_documents(docs)
+#    return splitDocs
 
 def create_vector_store(docs):
     embedding = OpenAIEmbeddings(api_key=openai_api_key)
@@ -65,14 +87,26 @@ def create_chain(vectorStore):
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
     return retrieval_chain
+    
+if st.button("Query Doc"):
+       if uploaded_file and user_question:
+           with st.spinner("Processing..."):
+              try:
+                  split_docs = get_docs_from_file(uploaded_file)
+                  vector_store = create_vector_store(split_docs)
+                  chain = create_chain(vector_store)
 
-docs = get_docs()
-vectorStore = create_vector_store(docs)
-chain = create_chain(vectorStore)
-
-response = chain.invoke({
-    "input": "What is LLM?",
-})
-
-st.write("After RAG Search, RESPONSE : ", response['answer'])
-#print(response)
+                  context = " ".join([doc.page_content for doc in split_docs])
+                  #st.write(f"Context: {context}")
+                  #st.write(f"Question: {user_question}")
+                  response = chain.invoke({"context": context, "input": user_question})
+                  #st.write("### Full Response")
+                  #st.write(response)
+                  if 'answer' in response:
+                      st.write("### Answer")
+                      st.write(response['answer'])
+              except Exception as e:
+                  st.error(f"An error occurred: {e}")
+       else:
+          st.warning("Please enter your OpenAI API Key")
+           
